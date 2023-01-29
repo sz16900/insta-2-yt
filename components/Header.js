38b67993dbccd3b1eaa49a9cc6841a2254bd904c
Seth Zea <sz16900@my.bristol.ac.uk>
2023-01-28 14:23:06 -1000

    finish header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,67 @@
 import Image from 'next/image';
 import React from 'react';
+import {
+  MagnifyingGlassIcon,
+  Bars3Icon,
+  PaperAirplaneIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+} from '@heroicons/react/24/outline';
+import { HomeIcon } from '@heroicons/react/24/solid';
 
 function Header() {
   return (
-    <div className="flex justify-betweeen bg-white max-w-6xl">
-      <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
-        <Image src="https://links.papareact.com/ocw" height={500} width={500} />
-      </div>
-      <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
-        <Image src="https://links.papareact.com/jjm" height={500} width={500} />
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            height={500}
+            width={500}
+          />
+        </div>
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            height={500}
+            width={500}
+          />
+        </div>
+        {/* Middle Seaerch Field*/}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-500 " />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <Bars3Icon className="h-6 md:hidden " />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn" />
+            <div className="absolute -top-2 -right-3 text-xs  w-5 h-5 bg-red-500 rounded-full flex text-white animate-pulse justify-center items-center">
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+
+          <img
+            src="https://links.papareact.com/3ke"
+            alt=""
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
